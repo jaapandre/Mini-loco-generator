fbfd18986d87bb93b10e22a64d9c3dbb51c110d3
Ben Peachey <potherca@gmail.com>
2012-02-19 21:19:45 +0100

    Refactored code into a Generator Class
    -- Various properties like $numberOfExercises and $resultSize=28 are now Class properties.
    -- The current Assignment and Layout are kept as Class Properties.
    -- The $layouts have been moved out of method scope and into Class scope.
    -- The various functions that populate DOMNodes with generated content have been moved to the Generator class too, although technically speaking it is not the Generators responsibility to fill them, so these will probably move to a different class in a later refactor round.

--- index.php
+++ index.php
@@ -17,74 +17,50 @@ require 'errorHandling.php';
 require 'locoImage.php';
 require 'class.Template.php';
 
-$numberOfExercises=12;
-$resultSize=28; // multiple of 4
-
-
-$layout=getLayout();
-
+$generator = new Generator();
 //------------------------------------------------------------------------------
 // Load Template
 $template = \Potherca\Template::fromFile('index.template.html');
 // Build Relevant Content Blocks
-
-function fetchAssignmentArray($numberOfExercises)
-{ //fill array with assignments
-    $assignmentTemp = $_POST['assignment'];
-    $outcomeTemp = $_POST['outcome'];
-
-    $assignment = array();
-    for ($i = 1; $i <= $numberOfExercises; $i++) {
-        $tempAssignment = $assignmentTemp[$i];
-        $tempOutcome = $outcomeTemp[$i];
-        //@FIXME: This creates a bug where, if two or more assignment have the same value, we overwrite $assignmentKeys and reduce the actual amount of keys available later on (also messing up the order.
-        $assignment[$tempAssignment] = $tempOutcome;
-    }
-    return $assignment; #for
-}
 
 if (empty($_POST['assignment'])){
     //no input so display form
 
     // Remove Views we don't want
     $template->getBody()->removeChild(
-        $template->getElementsByClassName('result-view')->item(0)
+        $template->getFirstElementWithClassName('result-view')
     );
 
     // Fetch nodes we'll work with
-    $resultTable = $template->getElementsByClassName('result-preview')->item(0);
-    $assignmentTable = $template->getElementsByClassName('assignment')->item(0);
+    $resultTable = $template->getFirstElementWithClassName('result-preview');
+    $assignmentTable = $template->getFirstElementWithClassName('assignment');
 
     // Build preview
-    buildResultPreview($layout, $resultSize, $resultTable, $template);
+    $generator->buildResultPreview($resultTable);
 
     // Add Assignment Form to Template
-    buildAssignmentContent($numberOfExercises, $template, $assignmentTable);
+    $generator->buildAssignmentContent($assignmentTable);
 } else {
     //input so display result
 
     // Remove Views we don't want
     $template->getBody()->removeChild(
-        $template->getElementsByClassName('form-view')->item(0)
+        $template->getFirstElementWithClassName('form-view')
     );
 
     // Fetch nodes we'll work with
-    $assignmentResultTable = $template->getElementsByClassName('assignment-result')->item(0);
-    $resultTable = $template->getElementsByClassName('images-result')->item(0);
-    $answersResultTable = $template->getElementsByClassName('answers-result')->item(0);
-
-
-    $assignment = fetchAssignmentArray($numberOfExercises);
-    $assignmentKeys = array_keys($assignment);
+    $assignmentResultTable = $template->getFirstElementWithClassName('assignment-result');
+    $resultTable = $template->getFirstElementWithClassName('images-result');
+    $answersResultTable = $template->getFirstElementWithClassName('answers-result');
 
     // Add Assignment Result to Template
-    buildAssignmentResult($assignmentResultTable, $template, $layout, $assignmentKeys);
+    $generator->buildAssignmentResult($assignmentResultTable);
 
     // Build result
-    buildResultPreview($layout, $resultSize, $resultTable, $template);
+    $generator->buildResultPreview($resultTable);
 
     //show the answers
-    buildResultContent($answersResultTable, $template, $layout, $assignmentKeys, $assignment);
+    $generator->buildResultContent($answersResultTable);
 }#if
 
 // Output Template
@@ -92,161 +68,257 @@ echo $template;
 //------------------------------------------------------------------------------
 
 
-
-function getLayout ($id=null){
-	//add here more layout, on which position should the tile come, first is the tile number, second one the position
-	//add 24 layouts for loco with 24 tiles
-	$layouts[]=array(1=>9, 2=>8,3=>11,4=>10,5=>5,6=>7,7=>4,8=>2,9=>12,10=>3,11=>6,12=>1);
-	$layouts[]=array(1=>11, 2=>10,3=>7,4=>8,5=>1,6=>9,7=>2,8=>4,9=>12,10=>5,11=>6,12=>3);
-	$layouts[]=array(1=>7, 2=>11,3=>9,4=>8,5=>4,6=>12,7=>1,8=>6,9=>10,10=>2,11=>3,12=>5);
-	$layouts[]=array(1=>2, 2=>4,3=>11,4=>7,5=>5,6=>9,7=>1,8=>10,9=>6,10=>8,11=>12,12=>3);
-	$layouts[]=array(1=>6, 2=>2,3=>3,4=>5,5=>9,6=>1,7=>11,8=>8,9=>4,10=>12,11=>10,12=>7);
-	//$layouts[]=array(1=>, 2=>,3=>,4=>,5=>,6=>,7=>,8=>,9=>,10=>,11=>,12=>);
-	
-	//choose a layout
-	
-	shuffle($layouts);
-	$layout=$layouts[0];
-	return $layout;
-}
-
-function buildResultPreview($layout, $resultSize, DOMElement $tableNode, \Potherca\Template $template) {
-
-    $allSlices = buildSlicesForLayout($layout);
-
-    addTilesToTableNode($allSlices, $resultSize, $tableNode, $template);
-}
-
-function addTilesToTableNode($allSlices, $resultSize, DOMElement $tableNode, \Potherca\Template $template)
+class Generator
 {
-    $tableBodyNode = $tableNode->getElementsByTagName('tbody')->item(0);
-    $template->removeChildrenFromNode($tableBodyNode);
+    protected $numberOfExercises=12;
 
+    protected $resultSize=28; // multiple of 4
 
-    foreach ($allSlices as  $key => $value) {
-        if ($key % 6 === 0) {
-            // first cell of this row of 6
-            $currentRow = $template->createElement('tr');
-            $tableBodyNode->appendChild($currentRow);
-        }#if
+    protected $layouts = array(
+        //add here more layout, on which position should the tile come, first is the tile number, second one the position
+        //add 24 layouts for loco with 24 tiles
+          1 => array(1=>9, 2=>8,3=>11,4=>10,5=>5,6=>7,7=>4,8=>2,9=>12,10=>3,11=>6,12=>1)
+        , 2 => array(1=>11, 2=>10,3=>7,4=>8,5=>1,6=>9,7=>2,8=>4,9=>12,10=>5,11=>6,12=>3)
+        , 3 => array(1=>7, 2=>11,3=>9,4=>8,5=>4,6=>12,7=>1,8=>6,9=>10,10=>2,11=>3,12=>5)
+        , 4 => array(1=>2, 2=>4,3=>11,4=>7,5=>5,6=>9,7=>1,8=>10,9=>6,10=>8,11=>12,12=>3)
+        , 5 => array(1=>6, 2=>2,3=>3,4=>5,5=>9,6=>1,7=>11,8=>8,9=>4,10=>12,11=>10,12=>7)
+//        , array(1=>, 2=>,3=>,4=>,5=>,6=>,7=>,8=>,9=>,10=>,11=>,12=>);
+    );
 
-        createTile($resultSize, $value);
+    protected $layout;
 
-        $tableCell = $template->createElement('td');
-        $tableCell->setAttribute('class', 'result');
+    protected $assignment;
 
-        $image = $template->createElement('img');
-        $image->setAttribute('src', 'images/'.$value.'.png');
+    function fetchAssignmentArray()
+    {
+        $assignment = array();
 
-        /** @noinspection PhpUndefinedVariableInspection */
-        $currentRow->appendChild($tableCell);
-        $tableCell->appendChild($image);
-    }#foreach
-}
+        $assignmentTemp = $_POST['assignment'];
+        $outcomeTemp = $_POST['outcome'];
 
-function buildSlicesForLayout($layout)
-{
-    $layoutFlip = array_flip($layout);
-    ksort($layoutFlip);
-    //flip rows..
-    $upperRow = array_slice($layoutFlip, 6, 6);
-    $lowerRow = array_slice($layoutFlip, 0, 6);
-    $allSlices = array_merge($upperRow, $lowerRow);
-    return $allSlices;
-}
+        for ($i = 1; $i <= $this->getNumberOfExercises(); $i++) {
+            $tempAssignment = $assignmentTemp[$i];
+            $tempOutcome = $outcomeTemp[$i];
+            //@FIXME: This creates a bug where, if two or more assignment have the same value, we overwrite $assignmentKeys and reduce the actual amount of keys available later on (also messing up the order.
+            $assignment[$tempAssignment] = $tempOutcome;
+        }#for
 
-function buildAssignmentContent($numberOfExercises, \Potherca\Template $template, \DOMElement $assignmentTable)
-{
-    $assignmentTableBody = $assignmentTable->getElementsByTagName('tbody')->item(0);
-    $template->removeChildrenFromNode($assignmentTableBody);
-
-    for ($i = 1; $i <= $numberOfExercises; $i++) {
-        // Add row to table
-        $currentRow = $template->createElement('tr');
-        $assignmentTableBody->appendChild($currentRow);
-
-        // Number
-        $numberCell = $template->createElement('td', $i);
-
-        // Assignment
-        $assignmentCell = $template->createElement('td');
-        $assignmentInputNode = $template->createElement('input');
-        $assignmentInputNode->setAttribute('type', 'text');
-        $assignmentInputNode->setAttribute('name', 'assignment[' . $i . ']');
-        $assignmentCell->appendChild($assignmentInputNode);
-
-        // Outcome
-        $outcomeCell = $template->createElement('td');
-        $outcomeInputNode = $template->createElement('input');
-        $outcomeInputNode->setAttribute('type', 'text');
-        $outcomeInputNode->setAttribute('name', 'outcome[' . $i . ']');
-        $outcomeCell->appendChild($outcomeInputNode);
-
-        // Add everything to the row
-        $currentRow->appendChild($numberCell);
-        $currentRow->appendChild($assignmentCell);
-        $currentRow->appendChild($outcomeCell);
+        return $assignment;
     }
-    #for
-}
+
+    function getRandomLayout(){
+        return $this->layouts[array_rand($this->layouts)];
+    }
+
+    function buildResultPreview(DOMElement $tableNode) {
+
+        $allSlices = $this->buildSlicesForLayout();
+
+        $this->addTilesToTableNode($allSlices, $tableNode);
+    }
+
+    function addTilesToTableNode($allSlices, DOMElement $tableNode)
+    {
+        $resultSize = $this->getResultSize();
+
+        $template = $tableNode->ownerDocument;
+
+        $tableBodyNode = $tableNode->getElementsByTagName('tbody')->item(0);
+        /** @noinspection PhpUndefinedMethodInspection Method removeChildrenFromNode() is defined in the Template Class*/
+        $template->removeChildrenFromNode($tableBodyNode);
 
 
+        foreach ($allSlices as  $key => $value) {
+            if ($key % 6 === 0) {
+                // first cell of this row of 6
+                $currentRow = $template->createElement('tr');
+                $tableBodyNode->appendChild($currentRow);
+            }#if
 
-function buildResultContent(DOMElement $tableNode, \Potherca\Template $template, $layout, $assignmentKeys, $assignment)
-{
-    $tableBodyNode = $tableNode->getElementsByTagName('tbody')->item(0);
-    $template->removeChildrenFromNode($tableBodyNode);
+            createTile($resultSize, $value);
 
-    $layoutFlip = array_flip($layout);
-    ksort($layoutFlip);
-    $count = 0;
-    foreach ($layoutFlip as $key => $value) {
-        $value--;
-        $assignmentKey = $assignmentKeys[$value];
-        if ($count % 6 === 0) {
+            // @TODO: $tableCell and $image should be created outside of the loop and cloned here as the only thing that changes is the images "src" attribute
+            $tableCell = $template->createElement('td');
+            $tableCell->setAttribute('class', 'result');
+
+            $image = $template->createElement('img');
+            $image->setAttribute('src', 'images/'.$value.'.png');
+
+            /** @noinspection PhpUndefinedVariableInspection */
+            $currentRow->appendChild($tableCell);
+            $tableCell->appendChild($image);
+        }#foreach
+    }
+
+    function buildSlicesForLayout()
+    {
+        $layout = $this->getLayout();
+
+        $layoutFlip = array_flip($layout);
+        ksort($layoutFlip);
+        //flip rows..
+        $upperRow = array_slice($layoutFlip, 6, 6);
+        $lowerRow = array_slice($layoutFlip, 0, 6);
+
+        $allSlices = array_merge($upperRow, $lowerRow);
+
+        return $allSlices;
+    }
+
+    function buildAssignmentContent(\DOMElement $assignmentTable)
+    {
+        $numberOfExercises = $this->getNumberOfExercises();
+
+        $template = $assignmentTable->ownerDocument;
+
+        $assignmentTableBody = $assignmentTable->getElementsByTagName('tbody')->item(0);
+        /** @noinspection PhpUndefinedMethodInspection Method removeChildrenFromNode() is defined in the Template Class*/
+        $template->removeChildrenFromNode($assignmentTableBody);
+
+        for ($i = 1; $i <= $numberOfExercises; $i++) {
+            // Add row to table
             $currentRow = $template->createElement('tr');
-            $tableBodyNode->appendChild($currentRow);
+            $assignmentTableBody->appendChild($currentRow);
+
+            // Number
+            $numberCell = $template->createElement('td', $i);
+
+            // @TODO: $assignmentCell and it's contents should be created outside of the loop and cloned here as the only thing that changes is the inputs "name" attribute
+            // Assignment
+            $assignmentCell = $template->createElement('td');
+            $assignmentInputNode = $template->createElement('input');
+            $assignmentInputNode->setAttribute('type', 'text');
+            $assignmentInputNode->setAttribute('name', 'assignment[' . $i . ']');
+            $assignmentCell->appendChild($assignmentInputNode);
+
+            // Outcome
+            $outcomeCell = $template->createElement('td');
+            $outcomeInputNode = $template->createElement('input');
+            $outcomeInputNode->setAttribute('type', 'text');
+            $outcomeInputNode->setAttribute('name', 'outcome[' . $i . ']');
+            $outcomeCell->appendChild($outcomeInputNode);
+
+            // Add everything to the row
+            $currentRow->appendChild($numberCell);
+            $currentRow->appendChild($assignmentCell);
+            $currentRow->appendChild($outcomeCell);
         }
-
-        $tableCell = $template->createElement('td', $assignment[$assignmentKey]);
-        $tableCell->setAttribute('class', 'answer');
-
-        /** @noinspection PhpUndefinedVariableInspection */
-        $currentRow->appendChild($tableCell);
-
-        $count++;
+        #for
     }
-    #foreach
-}
 
-function buildAssignmentResult(DOMElement $tableNode, \Potherca\Template $template, $layout, $assignmentKeys)
-{
-    $tableBodyNode = $tableNode->getElementsByTagName('tbody')->item(0);
-    $template->removeChildrenFromNode($tableBodyNode);
 
-    foreach ($layout as $key => $value) {
-        if (($key - 1) % 6 === 0) {
-            $currentRow = $template->createElement('tr');
-            $tableBodyNode->appendChild($currentRow);
+    function buildResultContent(DOMElement $tableNode)
+    {
+        $layout = $this->getLayout();
+        $template = $tableNode->ownerDocument;
+        $assignment = $this->getAssignment();
+        $assignmentKeys = $this->getAssignmentKeys();
+
+        $tableBodyNode = $tableNode->getElementsByTagName('tbody')->item(0);
+        /** @noinspection PhpUndefinedMethodInspection Method removeChildrenFromNode() is defined in the Template Class*/
+        $template->removeChildrenFromNode($tableBodyNode);
+
+        $layoutFlip = array_flip($layout);
+        ksort($layoutFlip);
+        $count = 0;
+        foreach ($layoutFlip as $key => $value) {
+            $value--;
+            $assignmentKey = $assignmentKeys[$value];
+            if ($count % 6 === 0) {
+                $currentRow = $template->createElement('tr');
+                $tableBodyNode->appendChild($currentRow);
+            }
+
+            $tableCell = $template->createElement('td', $assignment[$assignmentKey]);
+            $tableCell->setAttribute('class', 'answer');
+
+            /** @noinspection PhpUndefinedVariableInspection */
+            $currentRow->appendChild($tableCell);
+
+            $count++;
+        }
+        #foreach
+    }
+
+    function buildAssignmentResult(DOMElement $tableNode)
+    {
+        $assignmentKeys = $this->getAssignmentKeys();
+
+        $layout = $this->getLayout();
+        $template = $tableNode->ownerDocument;
+
+        $tableBodyNode = $tableNode->getElementsByTagName('tbody')->item(0);
+        /** @noinspection PhpUndefinedMethodInspection Method removeChildrenFromNode() is defined in the Template Class*/
+        $template->removeChildrenFromNode($tableBodyNode);
+
+        foreach ($layout as $key => $value) {
+            if (($key - 1) % 6 === 0) {
+                $currentRow = $template->createElement('tr');
+                $tableBodyNode->appendChild($currentRow);
+            }#if
+
+            $tableCell = $template->createElement('td');
+            $tableCell->setAttribute('class', 'assignment');
+
+            $font = $template->createElement('span', $key . ')');
+            $font->setAttribute('class', 'number');
+
+            //        $content .= "<br><br>";
+            $center = $template->createElement('center', $assignmentKeys[$key - 1]);
+            //        $content .= "<br><br>";
+
+            $tableCell->appendChild($font);
+            $tableCell->appendChild($center);
+
+            /** @noinspection PhpUndefinedVariableInspection */
+            $currentRow->appendChild($tableCell);
+        }#foreach
+    }
+
+    protected function getAssignmentKeys()
+    {
+        $assignment = $this->getAssignment();
+        $assignmentKeys = array_keys($assignment);
+        return $assignmentKeys;
+    }
+
+    public function setNumberOfExercises($numberOfExercises)
+    {
+        $this->numberOfExercises = $numberOfExercises;
+    }
+
+    public function getNumberOfExercises()
+    {
+        return $this->numberOfExercises;
+    }
+
+    public function setResultSize($resultSize)
+    {
+        $this->resultSize = $resultSize;
+    }
+
+    public function getResultSize()
+    {
+        return $this->resultSize;
+    }
+
+    public function getLayout()
+    {
+        if(!isset($this->layout)){
+            $this->layout = $this->getRandomLayout();
         }#if
 
-        $tableCell = $template->createElement('td');
-        $tableCell->setAttribute('class', 'assignment');
+        return $this->layout;
+    }
 
-        $font = $template->createElement('span', $key . ')');
-        $font->setAttribute('class', 'number');
+    public function getAssignment()
+    {
+        if(!isset($this->assignment)){
+            $this->assignment = $this->fetchAssignmentArray();
+        }#if
 
-        //        $content .= "<br><br>";
-        $center = $template->createElement('center', $assignmentKeys[$key - 1]);
-        //        $content .= "<br><br>";
+        return $this->assignment;
+    }
 
-        $tableCell->appendChild($font);
-        $tableCell->appendChild($center);
-
-        /** @noinspection PhpUndefinedVariableInspection */
-        $currentRow->appendChild($tableCell);
-    }#foreach
 }
-
-
 #EOF
